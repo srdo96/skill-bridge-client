9786d2d138bc5ecbcbec41738097ff00023a29b4
Sakib Rahman <seu.sakib@gmail.com>
2026-02-03 15:24:24 +0600

    setup env

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const env = createEnv({
+    server: { BACKEND_URL: z.url(), FRONTEND_URL: z.url() },
+    runtimeEnv: {
+        BACKEND_URL: process.env.BACKEND_URL,
+        FRONTEND_URL: process.env.FRONTEND_URL,
+    },
+});
