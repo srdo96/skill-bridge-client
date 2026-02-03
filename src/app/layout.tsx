137dfa67089e02bae1aac4cd61efc09db139c138
Sakib Rahman <seu.sakib@gmail.com>
2026-02-03 22:39:38 +0600

    implement registration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/provider/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -35,6 +36,7 @@ export default function RootLayout({
                     disableTransitionOnChange
                 >
                     {children}
+                    <Toaster richColors />
                 </ThemeProvider>
             </body>
         </html>
--- src/components/modules/authentication/register-form.tsx
+++ src/components/modules/authentication/register-form.tsx
@@ -1,48 +1,175 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import {
     Field,
     FieldDescription,
+    FieldError,
     FieldGroup,
     FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useForm } from "@tanstack/react-form";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import * as z from "zod";
+
+const formSchema = z.object({
+    name: z.string().min(1, "Full name is required"),
+    email: z.email(),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+});
 
 export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
+    const router = useRouter();
+    const form = useForm({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+        validators: {
+            onSubmit: formSchema,
+        },
+        onSubmit: async ({ value }) => {
+            const toastId = toast.loading("Creating account...");
+            try {
+                const { data, error } = await authClient.signUp.email({
+                    ...value,
+                    callbackURL: "http://localhost:3000/login",
+                });
+
+                if (error) {
+                    toast.error(error.message, { id: toastId });
+                    return;
+                }
+                toast.success("Account created successfully", { id: toastId });
+                router.push("/login");
+            } catch (error: any) {
+                toast.error(error.message, { id: toastId });
+                return;
+            }
+        },
+    });
     return (
         <Card {...props}>
             <CardHeader>
                 <CardTitle>Create an account</CardTitle>
+                <CardDescription>
+                    Enter your information below to create your account
+                </CardDescription>
             </CardHeader>
             <CardContent>
-                <form>
+                <form
+                    id="register-form"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        form.handleSubmit();
+                    }}
+                >
                     <FieldGroup>
-                        <Field>
-                            <FieldLabel htmlFor="name">Full Name</FieldLabel>
-                            <Input
-                                id="name"
-                                type="text"
-                                placeholder="John Doe"
-                                required
-                            />
-                        </Field>
-                        <Field>
-                            <FieldLabel htmlFor="email">Email</FieldLabel>
-                            <Input
-                                id="email"
-                                type="email"
-                                placeholder="m@example.com"
-                                required
-                            />
-                        </Field>
-                        <Field>
-                            <FieldLabel htmlFor="password">Password</FieldLabel>
-                            <Input id="password" type="password" required />
-                            <FieldDescription>
-                                Must be at least 8 characters long.
-                            </FieldDescription>
-                        </Field>
-
+                        <form.Field
+                            name="name"
+                            children={(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+                                return (
+                                    <Field>
+                                        <FieldLabel htmlFor="name">
+                                            Full Name
+                                        </FieldLabel>
+                                        <Input
+                                            type="text"
+                                            id={field.name}
+                                            name={field.name}
+                                            value={field.state.value}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value,
+                                                )
+                                            }
+                                            placeholder="John Doe"
+                                            required
+                                        />
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        />
+                        <form.Field
+                            name="email"
+                            children={(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+                                return (
+                                    <Field>
+                                        <FieldLabel htmlFor={field.name}>
+                                            Email
+                                        </FieldLabel>
+                                        <Input
+                                            type="email"
+                                            id={field.name}
+                                            name={field.name}
+                                            value={field.state.value}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        />
+                        <form.Field
+                            name="password"
+                            children={(field) => {
+                                const isInvalid =
+                                    field.state.meta.isTouched &&
+                                    !field.state.meta.isValid;
+                                return (
+                                    <Field>
+                                        <FieldLabel htmlFor={field.name}>
+                                            Password
+                                        </FieldLabel>
+                                        <Input
+                                            type="password"
+                                            id={field.name}
+                                            name={field.name}
+                                            value={field.state.value}
+                                            onChange={(e) =>
+                                                field.handleChange(
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
+                                        {isInvalid && (
+                                            <FieldError
+                                                errors={field.state.meta.errors}
+                                            />
+                                        )}
+                                    </Field>
+                                );
+                            }}
+                        />
                         <FieldGroup>
                             <Field>
                                 <Button type="submit">Create Account</Button>
