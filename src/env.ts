d67bfadfebab07431a8dea0038b232b7ec77b759
Sakib Rahman <seu.sakib@gmail.com>
2026-02-04 02:56:42 +0600

    implement proxy

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,10 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import z from "zod";
 
 export const env = createEnv({
-    server: { BACKEND_URL: z.url(), FRONTEND_URL: z.url() },
+    server: { BACKEND_URL: z.url(), FRONTEND_URL: z.url(), AUTH_URL: z.url() },
     runtimeEnv: {
         BACKEND_URL: process.env.BACKEND_URL,
         FRONTEND_URL: process.env.FRONTEND_URL,
+        AUTH_URL: process.env.AUTH_URL,
     },
 });
